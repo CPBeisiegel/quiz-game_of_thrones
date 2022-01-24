5039603c37181a0186559ce08d9bae45ed6d0023
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-01-24 10:31:14 -0300

    first commit with structure

---
+++ scripts/data.js
@@ -0,0 +1,73 @@
+const questions = [
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    },
+    {
+        question1: '',
+        alternative1: '',
+        alternative2: '',
+        alternative3: '',
+        alternative4: ''
+    }
+    
+];
---
+++ scripts/quiz.js
@@ -0,0 +1,27 @@
+// Fazer um classe que vai inserir as alternativas das perguntas e os métodos que vamos aplicar para o jogo 
+
+
+class questions{
+
+    constructor(){
+        
+    }
+
+    startGame(){
+
+    }
+
+    getTheQuestion(){
+
+    }
+
+    getTheAlternatives(){
+
+    }
+
+    showScore(){
+        // Aqui vou mostrar uma mensagem com o personagem de acordo com o número de alternativas no range 1 - 10000 
+    }
+
+    
+}
