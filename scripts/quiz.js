52a59165b43d171f2be56aeb24e53a9b2ed5adb5
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-01-25 17:01:24 -0300

    including changes in the code

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,31 +1,55 @@
 // Fazer um classe que vai inserir as alternativas das perguntas e os métodos que vamos aplicar para o jogo 
 
-class questions{
-    constructor(){
-        
+class Question{
+    constructor(text, alternatives, options){
+        this.text = text;
+        this.alternatives = alternatives;
+        this.options = options;
     }
+
+    isCorrectAnswer(alternative){
+        return this.alternative === alternative;
+    }
+
 }
 
-class quiz{
+class Quiz extends Question{
 
-    constructor(){
+    constructor(text, alternatives, options){
+        super(text, alternatives, options);
+        this.score = 0;
+        this.currentQuestionIndex = 0;
         
     }
 
     startGame(){
+        getCurrentQuestion(questions);
+        showScore()
 
     }
 
-    getTheQuestion(){
-
+    // Isso vai ser um callback para quando formos pegar as perguntas no arquivo data
+    getCurrentQuestion(questions){
+        return this.questions[this.currentQuestionIndex]
     }
 
-    getTheAlternatives(){
-
+    getRightAlternatives(answer){
+        if(this.currentQuestion().isCorrectAnswer(answer)){
+            this.score++
+        }
+        this.currentQuestionIndex++
     }
 
-    showScore(){
-        // Aqui vou mostrar uma mensagem com o personagem de acordo com o número de alternativas no range 1 - 10000 
+
+
+    showScore(score,progress){
+        // Aqui vou mostrar uma mensagem com o personagem de acordo com o número de alternativas no range 1 - 10 
+        getRightAlternatives(score)
+        this.progress = getCurrentQuestion(questions)        
+    }
+
+    endQuiz(){
+        return this.currentQuestion >= this.questions.length
     }
 
     
